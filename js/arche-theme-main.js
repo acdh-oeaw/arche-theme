aa371ea02672b3e4be7b55d0b5e3277f34586df6
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2024-03-05 11:31:43 +0100

    design changes, language switcher

--- js/arche-theme-main.js
+++ js/arche-theme-main.js
@@ -3,14 +3,48 @@ jQuery(function ($) {
 
 
     $(document).ready(function () {
+        /**
+         * Remove the user menu menupoint and just leave the logout button
+         */
         $('a[data-drupal-link-system-path="user"]').each(function () {
             $(this).parent('li').remove();
         });
 
+        //if the user press the browser back button, then we need to reload the page
+        // because of the ajax page refresh
+        $(window).on('popstate', function() {
+            $(".loader-div").show();
+            location.reload(true);
+        });
+
+        /**
+         * Check that the user changed the language on the gui, if yes then we do 
+         * a small api call, to change the drupal session language variable
+         */
+        $('.language-switcher-language-session-oeaw').on('change', function(event) {
+            
+            event.preventDefault();
+            let lng = $(this).val();
+            console.log(lng);
+            $.ajax({
+                url: '/browser/api/change_lng/'+lng,
+                type: "POST",
+                success: function(data, status) {
+                    console.log('success');
+                    location.reload();
+                },
+                error: function(message) {
+                    return message;
+                }
+            });
+            
+            
+        });
 
 
 
     });
+    
     $(document).delegate("a#footer-versions-btn", "click", function (e) {
         console.log('itt');
         e.preventDefault();
@@ -25,5 +59,8 @@ jQuery(function ($) {
         }
 
     });
+    
+    
+     
 
 });
